217205b7de562d147f88367c52636c39a5e0b956
shareef <shareefmohammedmm@gmail.com>
2023-02-10 10:39:29 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,53 @@
-import React from 'react';
+import React, { ChangeEvent, FC, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ITask } from './interfaces';
+import TodoTask from './components/TodoTask';
 
-function App() {
+
+const App: FC = () => {
+  const [task, setTask] = useState<string>('')
+  const [deadline, setDeadline] = useState<number>(0)
+  const [todoList, setTodo] = useState<ITask[]>([])
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    if(event.target.name === 'task'){
+      setTask(event.target.value)
+    } else {
+      setDeadline(Number(event.target.value))
+    }
+  }
+
+  const addTask = (): void => {
+    const newTask = { taskName: task, deadline: deadline}
+    setTodo([...todoList, newTask])
+    setDeadline(0)
+    setTask("")
+  }
+
+  const completeTask = (taskNameToDelete: string) : void => {
+    setTodo(todoList.filter((task) => {
+      return task.taskName != taskNameToDelete
+    }))
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='header'>
+        <div className='inputContainer'>
+          <input type='text' placeholder='Task...'  name='task' onChange={handleChange} value={task} />
+          <input type='number' value={deadline} placeholder='Dead line (in days)...' name='deadline' onChange={handleChange} />
+        </div>
+        <button onClick={addTask}>Add Task</button>
+      </div>
+      <div className='todoList' >
+        {
+          todoList.map((task:ITask, key:number) => {
+            return <TodoTask key={key} task={task} completeTask={completeTask} />
+          })
+        }
+      </div>
     </div>
   );
 }
